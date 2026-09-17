88b0182995ba6d7c546d545bd6e6f52f4c22271b
AntonTrofimov2025 <xerurganton@gmail.com>
2026-09-17 17:54:43 +0200

    Created reset_db.sh script to make migrations easier because of its sqlite origin in my project. It clears old sqlite migrations and prepares basement for MySQL. Very useful :)

--- reset_db.sh
+++ reset_db.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-find . -path "*/migrations/*.py" -not -name "__init__.py" -delete
+sudo find . -path "*/migrations/*.py" -not -name "__init__.py" -delete
 
 docker compose down
 sudo rm -rf ./db/*
