365b53cf0643ad96ef5234aa3e889ac18ca270a7
AntonTrofimov2025 <xerurganton@gmail.com>
2026-09-18 12:24:59 +0200

    Finally fine tuned all docker files and reset_db.sh script. All Docker deployment logic is now finished. Now everything thought through and works fine, as it should be :)

--- reset_db.sh
+++ reset_db.sh
@@ -11,17 +11,16 @@ mkdir -p logs media
 
 echo "Ожидаем инициализацию MySQL..."
 docker compose up -d db
-docker compose wait db --condition service_healthy
 
 docker compose run --rm migrate sh -c "python manage.py makemigrations users && \
  python manage.py makemigrations && python manage.py migrate users && python manage.py migrate"
 
+echo "Поднимаем весь проект..."
+echo "Ожидаем полную готовность веб-сервера..."
 docker compose up -d
 
-echo "Ожидаем полную готовность веб-сервера..."
-docker compose wait web --condition service_healthy
-
 echo "Настраиваем стили для NGINX..."
+sleep 5
 docker compose exec -T web python manage.py collectstatic --noinput
 
 echo "Done!! :)"
