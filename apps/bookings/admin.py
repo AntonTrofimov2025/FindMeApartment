cb4c54fbe42c135749d41a9a52e13609ff03b8ca
AntonTrofimov2025 <xerurganton@gmail.com>
2026-09-19 14:24:55 +0200

    Updated bookings admin.py and improved ping endpoint.

--- apps/bookings/admin.py
+++ apps/bookings/admin.py
@@ -9,7 +9,7 @@ class BookingAdmin(admin.ModelAdmin):
         'id', 'listing', 'user', 'date_from', 'date_to', 'booking_status', 'guests_number',
         'total_price', 'created_at', 'updated_at', 'show_is_deleted'
     )
-    readonly_fields = ('deleted_at', "snapshot_data")
+    readonly_fields = ('deleted_at', "snapshot_data", "total_price")
     search_fields = ('listing__title', 'listing__city', 'listing__country', 'user__email')
     list_filter = ('booking_status', 'date_from', 'date_to')
     ordering = ('-created_at',)
