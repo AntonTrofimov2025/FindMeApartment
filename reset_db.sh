62969f0635df2e943cc83430dac9ac9edde8e754
AntonTrofimov2025 <xerurganton@gmail.com>
2026-09-19 13:26:42 +0200

    Made two separate docker-compose.yml(with and without migrate service) and updated reset_db.sh. Now you do not have to wait migrations again after you use docker compose up -d command. :)

--- reset_db.sh
+++ reset_db.sh
@@ -2,22 +2,22 @@
 
 sudo find . -path "*/migrations/*.py" -not -name "__init__.py" -delete
 
-docker compose down
+docker compose -f docker-compose.yml -f docker-compose-with-migrate.yml down
 sudo rm -rf ./db/*
 
-docker compose build --no-cache
+docker compose -f docker-compose-with-migrate.yml build --no-cache
 
 mkdir -p logs media
 
 echo "Ожидаем инициализацию MySQL..."
-docker compose up -d db
+docker compose -f docker-compose-with-migrate.yml up -d db
 
-docker compose run --rm migrate sh -c "python manage.py makemigrations users && \
+docker compose -f docker-compose-with-migrate.yml run --rm migrate sh -c "python manage.py makemigrations users && \
  python manage.py makemigrations && python manage.py migrate users && python manage.py migrate"
 
 echo "Поднимаем весь проект..."
 echo "Ожидаем полную готовность веб-сервера..."
-docker compose up -d
+docker compose -f docker-compose.yml up -d
 
 echo "Настраиваем стили для NGINX..."
 sleep 5
