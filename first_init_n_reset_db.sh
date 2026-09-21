e03d8bcf7d7bd63e9faf9245758b28c8ed4da128
AntonTrofimov2025 <xerurganton@gmail.com>
2026-09-21 13:18:28 +0200

    Extended script logic and renamed to first_init_n_reset_db.sh

--- first_init_n_reset_db.sh
+++ first_init_n_reset_db.sh
@@ -7,6 +7,8 @@ else
     exit 1
 fi
 
+set -e
+
 sudo find . -path "*/migrations/*.py" -not -name "__init__.py" -delete
 
 docker compose -f docker-compose.yml -f docker-compose-with-migrate.yml down
@@ -33,6 +35,6 @@ docker compose exec -T web python manage.py collectstatic --noinput
 echo "Выдаем полные права пользования на MySQL пользователю ${DB_USER}"
 sleep 5
 docker compose exec db mysql -u root -p"${DB_ROOT_PASSWORD}" -e "GRANT ALL PRIVILEGES ON \
- \`test_${DB_NAME}\`.* TO '${DB_USER}'@'%'; FLUSH PRIVILEGES;"
+test_${DB_NAME}.* TO '${DB_USER}'@'%'; FLUSH PRIVILEGES;"
 
 echo "Done!! :)"
