c1172d084d32659799b64f7304ed1c307edf088e
AntonTrofimov2025 <xerurganton@gmail.com>
2026-09-21 12:59:23 +0200

    Extended script logic and renamed to first_init_n_reset_db.sh

--- first_init_n_reset_db.sh
+++ first_init_n_reset_db.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+if [ -f .env ]; then
+    export $(grep -v '^#' .env | xargs)
+else
+    echo "Ошибка: Файл .env не найден!"
+    exit 1
+fi
+
 sudo find . -path "*/migrations/*.py" -not -name "__init__.py" -delete
 
 docker compose -f docker-compose.yml -f docker-compose-with-migrate.yml down
@@ -22,5 +29,10 @@ docker compose -f docker-compose.yml up -d
 echo "Настраиваем стили для NGINX..."
 sleep 5
 docker compose exec -T web python manage.py collectstatic --noinput
+
+echo "Выдаем полные права пользования на MySQL пользователю ${DB_USER}"
+sleep 5
+docker compose exec db mysql -u root -p"${DB_ROOT_PASSWORD}" -e "GRANT ALL PRIVILEGES ON \
+ \`test_${DB_NAME}\`.* TO '${DB_USER}'@'%'; FLUSH PRIVILEGES;"
 
 echo "Done!! :)"
